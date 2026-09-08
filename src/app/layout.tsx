2466338b2bf04d06bea0dc05155e134bb476dcaf
Sistemas Vepagos <sistemas@somosvepagos.com>
2026-09-08 10:44:32 -0400

    feat: initialize root layout with Barlow fonts and configure npm legacy peer dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,9 @@ export default function RootLayout({
     <html
       lang="es"
       className={`${barlow.variable} ${barlowCondensed.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
